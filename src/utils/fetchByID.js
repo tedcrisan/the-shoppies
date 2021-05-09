63c097c0b6f4578a292c0b70e2b4d8181728e2d7
Ted Crisan <tedcrisan@live.ca>
2021-05-09 18:07:17 -0400

    Change api path for fetchByID function

--- src/utils/fetchByID.js
+++ src/utils/fetchByID.js
@@ -4,7 +4,7 @@ export default async function fetchByID(id) {
   const API_KEY = process.env.NEXT_PUBLIC_OMDB_API_KEY;
   try {
     const response = await axios.get(
-      `http://www.omdbapi.com/?i=${id}&type=movie&apikey=${API_KEY}`
+      `https://www.omdbapi.com/?i=${id}&type=movie&apikey=${API_KEY}`
     );
     return { movie: response.data, err: false };
   } catch (err) {
